a09ed6dd0f66db1ad1580ff72d43f4623068dcbb
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-06-15 13:37:07 +0200

    Integration tests fixed

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -6,12 +6,12 @@ dataSource {
     password = ""
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
+//    cache.use_second_level_cache = true
+//    cache.use_query_cache = false
 //    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
-    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
-    singleSession = true // configure OSIV singleSession mode
-    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+//    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
+//    singleSession = true // configure OSIV singleSession mode
+//    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
 
 // environment specific settings
