a027ce0a59f9723dece9c9834f5455ce28f8229e
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-06-15 17:31:18 +0200

    Added teses for money:format

--- test/unit/com/ticketbis/money/MoneyTagLibSpec.groovy
+++ test/unit/com/ticketbis/money/MoneyTagLibSpec.groovy
@@ -2,6 +2,8 @@ package com.ticketbis.money
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 
 /**
  * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
@@ -39,4 +41,33 @@ class MoneyTagLibSpec extends Specification {
         html =~ /name="testMoney_amount".*value="123.45"/
         html =~ /name="testMoney_currency".*value="USD"/
     }
+
+    void "test money formatting"() {
+    given:
+        def money = new Money(1234.5G, Currency.getInstance('USD'))
+
+    expect:
+        'USD 1,234.50' == applyTemplate(
+            '<money:format value="${ money }" pattern="¤ ##,##0.00"/>', [money: money])
+    }
+
+    void "test money advanced formatting"() {
+    given:
+        DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+        dfs.currencySymbol = 'руб.'
+        dfs.groupingSeparator = ' '
+        dfs.decimalSeparator = ','
+
+        def formatter = new DecimalFormat("##,##0 ¤")
+        formatter.decimalFormatSymbols = dfs
+
+        def money = new Money(1234.0G, 'RUB')
+
+    expect:
+        '1 234 руб.' == applyTemplate(
+            '<money:format value="${ money }" numberFormat="${ formatter }"/>',
+            [money: money, formatter: formatter])
+
+    }
+
 }
