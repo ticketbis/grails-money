b4aa87a9836b257f63d7a983b5048afd55a14315
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-07-02 12:18:29 +0200

    StructuredMoneyEditor now can get numberFormat with symbols

--- src/groovy/com/ticketbis/money/StructuredMoneyEditor.groovy
+++ src/groovy/com/ticketbis/money/StructuredMoneyEditor.groovy
@@ -1,30 +1,37 @@
 package com.ticketbis.money
 
 import org.grails.databinding.converters.AbstractStructuredBindingEditor
-
 import org.springframework.context.i18n.LocaleContextHolder as LCH
-
 import java.text.DecimalFormat
 import java.text.NumberFormat
+import java.text.DecimalFormatSymbols
+import groovy.transform.CompileStatic
 
+@CompileStatic
 class StructuredMoneyEditor extends AbstractStructuredBindingEditor<Money> {
 
     Money getPropertyValue(Map values) {
-
         // retrieve the individual values from the Map
-        def amount = values.amount
-        def currency = values.currency
+        String amount = values.amount
+        String currency = values.currency
 
         // build decimal formatter
         DecimalFormat formatter
-        if (values.parseFormat) {
-            formatter = new DecimalFormat(values.parseFormat)
+        if (values.numberFormatter) {
+            Map numberFormat = (Map) values.numberFormat
+            formatter = new DecimalFormat((String) numberFormat.pattern)
+
+            DecimalFormatSymbols dfs = new DecimalFormatSymbols()
+            dfs.decimalSeparator = (char) numberFormat.decimalSeparator
+            dfs.groupingSeparator = (char) numberFormat.groupingSeparator
+            dfs.currencySymbol = numberFormat.currencySymbol
+            formatter.decimalFormatSymbols = dfs
         } else {
             formatter = getLocaleAwareDecimalFormatter()
         }
-        formatter.setParseBigDecimal(true)
+        formatter.parseBigDecimal = true
 
-        BigDecimal parsedAmount = formatter.parse(amount)
+        BigDecimal parsedAmount = (BigDecimal) formatter.parse(amount)
 
         // create and return a Money
         new Money(parsedAmount, currency)
