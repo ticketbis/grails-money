9ed3db5eb619ae2a6548435a55fe3442a676b2df
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-08-21 14:18:05 +0200

    Version set to 0.1.7

--- MoneyGrailsPlugin.groovy
+++ MoneyGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import com.ticketbis.money.NumberMoneyExtension
 
 class MoneyGrailsPlugin {
     // the plugin version
-    def version = "0.1.6"
+    def version = "0.1.7"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
